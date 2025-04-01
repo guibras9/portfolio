c65bcaec0506f65aef1ec325a12218217099745b
Guilherme Brás <gui.cenasrandom@gmail.com>
2025-04-01 20:57:47 +0100

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {Footer} from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar/>     
       <Banner/>
       <About/>
       <Experience/>
